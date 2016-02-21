99c4423d5e3cf852325616dd4a72aa1b6fc39f7d
Brian Steffens <briansteffens@gmail.com>
2016-02-21 14:06:06 -0500

    Add game list

--- src/server.js
+++ src/server.js
@@ -1,13 +1,35 @@
 var fs = require('fs');
 
 var sessions = {
-  0: {
-    username: 'brian'
+  'a': {
+    user_id: 3,
   },
-  1: {
-    username: 'evilbrian'
+  'b': {
+    user_id: 7,
   },
 };
+
+var users = {
+  3: {
+    username: 'brian',
+  },
+  7: {
+    username: 'evilbrian',
+  },
+};
+
+var games = [
+  {
+    id: 0,
+    name: 'The first game ever!',
+    players: [3, 7],
+  },
+  {
+    id: 1,
+    name: 'The second game ever',
+    players: [7, 3],
+  },
+];
 
 require('http').createServer(function(req, res) {
   var url = require('url').parse(req.url, true);
@@ -28,7 +50,29 @@ require('http').createServer(function(req, res) {
       'Set-Cookie': 'session_id=' + url.query.session,
     });
 
-    fs.createReadStream(path).pipe(res)
+    return fs.createReadStream(path).pipe(res);
+  }
+
+  var cookies = require('cookie').parse(req.headers.cookie);
+  var session = sessions[cookies.session_id];
+
+  if (url.pathname === '/games') {
+    var game_list = [];
+
+    for (var i = 0; i < games.length; i++) {
+      if (games[i].players.indexOf(session.user_id) >= 0) {
+        game_list.push(games[i]);
+      }
+    }
+
+    var json = JSON.stringify({games: game_list});
+
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+      'Content-Length': json.length,
+    });
+
+    res.end(json);
   }
   else {
     res.writeHead(404);
